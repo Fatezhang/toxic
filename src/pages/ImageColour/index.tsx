fbd8310984d8155d7c0ef8c462ebe478bc9ccc47
Jiaheng <zhangjiahengpoping@gmail.com>
2023-05-05 10:06:29 +0700

    feat: create a new page

--- src/pages/ImageColour/index.tsx
+++ src/pages/ImageColour/index.tsx
@@ -1,5 +1,18 @@
 const ImageColour = () => {
-  return <>1234</>;
+  return (
+    <div className="p-2 h-full">
+      <div className="flex flex-row h-full bg-white card shadow-2xl shadow-gray-500">
+        <div className="w-1/2 h-full flex items-center">
+          <div className="h-2/5 bg-black rounded-r-lg w-full"/>
+        </div>
+        <div className="w-1/2 h-full flex items-center p-5 gap-24 justify-center">
+          <div className="w-20 h-20 border bg-black rounded"/>
+          <div className="w-20 h-20 border bg-black rounded"/>
+          <div className="w-20 h-20 border bg-black rounded"/>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { ImageColour };
